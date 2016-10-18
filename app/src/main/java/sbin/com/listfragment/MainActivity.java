ec1bee5628f1ffcfeb8afff5a05ec06c936d9a1c
bindolee <bindolee@gmail.com>
2016-10-18 16:51:17 -0700

    Made FragmentStatePagerAdapter to display fragment list

--- app/src/main/java/sbin/com/listfragment/MainActivity.java
+++ app/src/main/java/sbin/com/listfragment/MainActivity.java
@@ -1,38 +1,51 @@
 package sbin.com.listfragment;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final List<Product> product = DataProvider.productList;
-    private final int numPages = product.size();
+    private final List<Product> products = DataProvider.productList;
+    private final int numPages = products.size();
+    private ViewPager mPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mPager = (ViewPager) findViewById(R.id.pager);
+        PagerAdapter pagerAdapter =
+                new ViewPagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(pagerAdapter);
     }
 
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        //This gets called each time I navigate to page.
+        @Override
+        public Fragment getItem(int position) {
+            return ItemFragment.newInstance(products.get(position));
+        }
+
+        @Override
+        public int getCount() {
+            return numPages;
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
